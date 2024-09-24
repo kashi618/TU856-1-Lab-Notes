760c2942f397760eaa28204149eb3f0557702bde
kashi618 <nialj618@gmail.com>
2024-09-24 09:54:07 +0100

    sync

--- Programming/Sep 24th 2024/CA_Assignment.c
+++ Programming/Sep 24th 2024/CA_Assignment.c
@@ -1,16 +1,26 @@
+/*
+Program Description:    This program declares and assigns 3 variables with values, and shows them to the user.
+.                      
+
+Author:                 C24510496 | Neil Jiang
+
+Date:                   September 24, 2024
+*/
+
 #include <stdio.h>
 
 int main()
 {
-    //Program to illustrate errors in a C program
-
+    // Declares Variables
     int   num1, num2;
     float num3;
 
+    // Assigns Value to Variables
     num1 = 400;
     num2 = 600;
     num3 = num2;
 
+    // Shows Each Variable
     printf("num1 contains %d ", num1);
     printf("num2 contains %f and num3 contains %f", num2, num3);
 
